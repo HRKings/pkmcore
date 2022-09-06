3ad1e4196618f5935327ffa6eafd4dc30147c4cc
Helton Reis <hrkings@rhaizzing.com>
2022-09-06 17:26:59 -0300

    feat(game/enums): add pokemon genders

---
+++ src/game/enums/pokemon_gender.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone)]
+pub enum PokemonGender {
+	Male = 0,
+	Female = 1,
+	Genderless = 2,
+}
+
+impl From<u8> for PokemonGender {
+    fn from(item: u8) -> Self {
+        match item {
+			2 => PokemonGender::Genderless,
+			1 => PokemonGender::Female,
+			_ => PokemonGender::Male
+		}
+    }
+}

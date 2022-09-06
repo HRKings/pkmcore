59404319c7a0548f9f2747e3d289eb1d7aae224e
Helton Reis <hrkings@rhaizzing.com>
2022-09-06 17:25:11 -0300

    chore: add game/enum mod

---
+++ src/game/enums/mod.rs
@@ -0,0 +1,6 @@
+pub mod game_version;
+pub mod pokemon_gender;
+pub mod pokemon_nature;
+pub mod language_id;
+pub mod species_id;
+pub mod gamecube_region;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod game;
+
 #[cfg(test)]
 mod tests {
     #[test]

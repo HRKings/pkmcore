290ea6fcaaf00512345e60cb6db622b1467e69ed
Helton Reis <hrkings@rhaizzing.com>
2022-09-06 17:29:44 -0300

    feat(utils): add global constants

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod utils;
 pub mod game;
 
 #[cfg(test)]
---
+++ src/utils/constants.rs
@@ -0,0 +1,3 @@
+pub const MAGIC_RATIO_GENDERLESS: u8 = 255;
+pub const MAGIC_RATIO_FEMALE: u8 = 254;
+pub const MAGIC_RATIO_MALE: u8 = 0;

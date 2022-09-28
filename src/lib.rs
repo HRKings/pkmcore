35094279f89a6c7e90ee6c5faae4f2f0d83e6f3e
Helton Reis <hrkings@rhaizzing.com>
2022-09-27 23:27:05 -0300

    feat: remove example test

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,5 @@ pub mod pokemon;
 pub mod species;
 pub mod utils;
 pub mod game;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
 pub mod save;
 pub mod mystery_gifts;

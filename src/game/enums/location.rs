bdd735462317e31455192ecd1fe15a9900ef43b2
Helton Reis <hrkings@rhaizzing.com>
2022-09-27 23:24:39 -0300

    feat(game/enums)!: make locations constants intead of a enum to allow duplicated values

--- src/game/enums/location.rs
+++ src/game/enums/location.rs
@@ -1,106 +1,104 @@
-pub enum Locations {
-  LinkTrade4 = 2002,
-  LinkTrade5 = 30003,
-  LinkTrade6 = 30002,
+pub const LINK_TRADE_GENERATION4: u16 = 2002;
+pub const LINK_TRADE_GENERATION5: u16 = 30003;
+pub const LINK_TRADE_GENERATION6: u16 = 30002;
 
-  Daycare4 = 2000,
-  Daycare5 = 60002,
-  Daycare8b = 60010,
+pub const DAYCARE_GENERATION4: u16 = 2000;
+pub const DAYCARE_GENERATION5: u16 = 60002;
+pub const DAYCARE_GENERATION8B: u16 = 60010;
 
-  LinkTrade2NPC = 126,
-  LinkTrade3NPC = 254,
-  LinkTrade4NPC = 2001,
-  LinkTrade5NPC = 30002,
-  LinkTrade6NPC = 30001,
+pub const LINK_TRADE_GENERATION2_NPC: u16 = 126;
+pub const LINK_TRADE_GENERATION3_NPC: u16 = 254;
+pub const LINK_TRADE_GENERATION4_NPC: u16 = 2001;
+pub const LINK_TRADE_GENERATION5_NPC: u16 = 30002;
+pub const LINK_TRADE_GENERATION6_NPC: u16 = 30001;
 
-  Breeder5 = 60003,
-  Breeder6 = 60004,
+pub const BREEDER_GENERATION5: u16 = 60003;
+pub const BREEDER_GENERATION6: u16 = 60004;
 
-  PokeWalker4 = 233,
-  Ranger4 = 3001,
-  Faraway4 = 3002,
+pub const POKE_WALKER_GENERATION4: u16 = 233;
+pub const RANGER_GENERATION4: u16 = 3001;
+pub const FARAWAY_GENERATION4: u16 = 3002;
 
-  /// Goldenrod City in Crystal
-  HatchLocation_GoldenrodCity_Crystal = 16,
+/// Goldenrod City in Crystal
+pub const HATCH_LOCATION_GOLDENROD_CITY_CRYSTAL: u16 = 16;
 
-  /// Route 117 in Ruby/Sapphire/Emerald
-  HatchLocation_Route117_RubySapphireEmerald = 32,
+/// Route 117 in Ruby/Sapphire/Emerald
+pub const HATCH_LOCATION_ROUTE117_RUBY_SAPPHIRE_EMERALD: u16 = 32;
 
-  /// Four Island in FireRed/LeafGreen
-  HatchLocation_FourIsland_FireRedLeafGreen = 146,
+/// Four Island in FireRed/LeafGreen
+pub const HATCH_LOCATION_FOUR_ISLAND_FIRE_RED_LEAF_GREEN: u16 = 146;
 
-  /// Solaceon Town in Diamond/Pearl/Platinum
-  HatchLocation_SolaceonTown_DiamondPearlPlatinum = 4,
+/// Solaceon Town in Diamond/Pearl/Platinum
+pub const HATCH_LOCATION_SOLACEON_TOWN_DIAMOND_PEARL_PLATINUM: u16 = 4;
 
-  /// Route 34 in HeartGold/SoulSilver
-  HatchLocation_Route34_HeartGold_SoulSilver = 182,
+/// Route 34 in HeartGold/SoulSilver
+pub const HATCH_LOCATION_ROUTE34_HEART_GOLD_SOUL_SILVER: u16 = 182;
 
-  /// Skyarrow Bridge in Black/White/Black2/White2
-  HatchLocation_SkyarrowBridge_BlackWhite_Black2White2 = 64,
+/// Skyarrow Bridge in Black/White/Black2/White2
+pub const HATCH_LOCATION_SKYARROW_BRIDGE_BLACK_WHITE_BLACK2WHITE2: u16 = 64;
 
-  /// Route 7 in X/Y
-  HatchLocation_Route7_XY = 38,
+/// Route 7 in X/Y
+pub const HATCH_LOCATION_ROUTE7_XY: u16 = 38;
 
-  /// Battle Resort in OmegaRuby/AlphaSapphire
-  HatchLocation_BattleResort_OmegaRubyAlphaSapphire = 318,
+/// Battle Resort in OmegaRuby/AlphaSapphire
+pub const HATCH_LOCATION_BATTLE_RESORT_OMEGA_RUBY_ALPHA_SAPPHIRE: u16 = 318;
 
-  /// Paniola Ranch in Sun/Moon/UltraSun/UltraMoon
-  HatchLocation_PaniolaRanch_SunMoon_UltraSunUltraMoon = 78,
+/// Paniola Ranch in Sun/Moon/UltraSun/UltraMoon
+pub const HATCH_LOCATION_PANIOLA_RANCH_SUN_MOON_ULTRA_SUN_ULTRA_MOON: u16 = 78;
 
-  /// Route 5 in Sword/Shield
-  HatchLocation_Route5_SwordShield = 40,
+/// Route 5 in Sword/Shield
+pub const HATCH_LOCATION_ROUTE5_SWORD_SHIELD: u16 = 40;
 
-  /// Solaceon Town in BrilliantDiamond/ShiningPearl
-  HatchLocation_SolaceonTown_BrilliantDiamondShiningPearl = 446,
+/// Solaceon Town in BrilliantDiamond/ShiningPearl
+pub const HATCH_LOCATION_SOLACEON_TOWN_BRILLIANT_DIAMOND_SHINING_PEARL: u16 = 446;
 
-  /// Generation 1 -> Generation 7 Transfer Location (Kanto)
-  Transfer_Generation1_Generation7 = 30013,
+/// Generation 1 -> Generation 7 Transfer Location (Kanto)
+pub const TRANSFER_GENERATION1_GENERATION7: u16 = 30013;
 
-  /// Generation 2 -> Generation 7 Transfer Location (Johto)
-  Transfer_Generation2_Generation7 = 30017,
+/// Generation 2 -> Generation 7 Transfer Location (Johto)
+pub const TRANSFER_GENERATION2_GENERATION7: u16 = 30017;
 
-  /// Generation 3 -> Generation 4 Transfer Location (Pal Park)
-  Transfer_PalPark_Generation3_Generation4 = 0x37,
+/// Generation 3 -> Generation 4 Transfer Location (Pal Park)
+pub const TRANSFER_PAL_PARK_GENERATION3_GENERATION4: u16 = 0x37;
 
-  /// Generation 4 -> Generation 5 Transfer Location (Poké Transporter)
-  Transfer_PokeTransporter_Generation4_Generation5 = 30001,
+/// Generation 4 -> Generation 5 Transfer Location (Poké Transporter)
+pub const TRANSFER_POKETRANSPORTER_GENERATION4_GENERATION5: u16 = 30001;
 
-  /// Generation 4 -> Generation 5 Transfer Location (Crown Celebi - Event not activated in Gen 5)
-  Transfer_CelebiUnused_Generation4_Generation5 = 30010,
+/// Generation 4 -> Generation 5 Transfer Location (Crown Celebi - Event not activated in Gen 5)
+pub const TRANSFER_CELEBI_UNUSED_GENERATION4_GENERATION5: u16 = 30010;
 
-  /// Generation 4 -> Generation 5 Transfer Location (Crown Celebi - Event activated in Gen 5)
-  Transfer_CelebiUsed_Generation4_Generation5 = 30011,
+/// Generation 4 -> Generation 5 Transfer Location (Crown Celebi - Event activated in Gen 5)
+pub const TRANSFER_CELEBI_USED_GENERATION4_GENERATION5: u16 = 30011;
 
-  /// Generation 4 -> Generation 5 Transfer Location (Crown Beast - Event not activated in Gen 5)
-  Transfer_CrownUnused_Generation4_Generation5 = 30012,
+/// Generation 4 -> Generation 5 Transfer Location (Crown Beast - Event not activated in Gen 5)
+pub const TRANSFER_CROWN_UNUSED_GENERATION4_GENERATION5: u16 = 30012;
 
-  /// Generation 4 -> Generation 5 Transfer Location (Crown Beast - Event activated in Gen 5)
-  Transfer_CrownUsed_Generation4_Generation5 = 30013,
+/// Generation 4 -> Generation 5 Transfer Location (Crown Beast - Event activated in Gen 5)
+pub const TRANSFER_CROWN_USED_GENERATION4_GENERATION5: u16 = 30013;
 
-  /// Generation 6 Gift from Pokémon Link
-  LinkGift_Generation6 = 30011,
+/// Generation 6 Gift from Pokémon Link
+pub const LINK_GIFT_GENERATION6: u16 = 30011;
 
-  /// Generation 7 Poké Pelago
-  PokePelago_Generation7 = 30016,
+/// Generation 7 Poké Pelago
+pub const POKE_POKEPELAGO_GENERATION7: u16 = 30016;
 
-  /// Generation 7 Transfer from GO to Pokémon LGP/E's GO Park
-  Transfer_Go_LetsGoEeveeLetsGoPikachu = 50,
+/// Generation 7 Transfer from GO to Pokémon LGP/E's GO Park
+pub const TRANSFER_GO_LETS_GO_EEVEE_LETS_GO_PIKACHU: u16 = 50;
 
-  /// Generation 8 Transfer from GO to Pokémon HOME
-  Transfer_Go_Home = 30012,
+/// Generation 8 Transfer from GO to Pokémon HOME
+pub const TRANSFER_GO_HOME: u16 = 30012;
 
-  /// Generation 8 Gift from Pokémon HOME
-  Gift_Home_Generation8 = 30018,
+/// Generation 8 Gift from Pokémon HOME
+pub const GIFT_HOME_GENERATION8: u16 = 30018;
 
-  BugCatchingContest_Generation4 = 207,
+pub const BUG_CATCHING_CONTEST_GENERATION4: u16 = 207;
 
-  /// SP traded to (SW)SH
-  HOME_ShiningPearl_SwordShield = 59998,
-  /// BD traded to SW(SH)
-  HOME_BrilliantDiamond_SwordShield = 59999,
-  /// PLA traded to SW(SH)
-  HOME_LegendsArceus_SwordShield = 60000,
-  /// -2 = 8bNone-1..
-  HOME_SWSHBDSPEgg = 65534,
-  Default8bNone = 65535,
-}
+/// SP traded to (SW)SH
+pub const HOME_SHINING_PEARL_SWORD_SHIELD: u16 = 59998;
+/// BD traded to SW(SH)
+pub const HOME_BRILLIANT_DIAMOND_SWORD_SHIELD: u16 = 59999;
+/// PLA traded to SW(SH)
+pub const HOME_LEGENDS_ARCEUS_SWORD_SHIELD: u16 = 60000;
+/// -2: u16 = 8bNone-1..
+pub const HOME_SWSHBDSPEGG: u16 = 65534;
+pub const DEFAULT8B_NONE: u16 = 65535;
